4da146ad9f28f67519e57a4b3365e80bacd91dea
khaily <ly.k@husky.neu.edu>
2020-03-21 14:57:04 -0700

    ng g c created coursetable, courseviews, lessontabs, and modulelist components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
+import { CourseTableComponentComponent } from './course-table-component/course-table-component.component';
+import { CourseViewerComponentComponent } from './course-viewer-component/course-viewer-component.component';
+import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
+import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tabs-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    CourseTableComponentComponent,
+    CourseViewerComponentComponent,
+    ModuleListComponentComponent,
+    LessonTabsComponentComponent
   ],
   imports: [
     BrowserModule,
