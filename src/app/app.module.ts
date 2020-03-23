d828d585cc93df0d29d6bfc1ab6b4577fdfb2617
khaily <ly.k@husky.neu.edu>
2020-03-23 14:34:51 -0700

    added declarations in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseTableComponent } from './course-table/course-table.component';
+import {CourseServiceClient} from './services/CourseServiceClient';
+import {FormsModule} from '@angular/forms';
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { ModuleListComponent } from './module-list/module-list.component';
+import {ModuleServiceClient} from './services/ModuleServiceClient';
 import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import {LessonServiceClient} from './services/LessonServiceClient';
 
 @NgModule({
   declarations: [
@@ -14,13 +18,18 @@ import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
     CourseTableComponent,
     CourseViewerComponent,
     ModuleListComponent,
-    LessonTabsComponent
+    LessonTabsComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CourseServiceClient,
+    ModuleServiceClient,
+    LessonServiceClient,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/course-table/course-table.component.ts
+++ src/app/course-table/course-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CourseServiceClient} from '../services/CourseServiceClient';
 
 @Component({
   selector: 'app-course-table',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CourseServiceClient) { }
 
+  courseTitle = '';
+  courses = [
+    {_id: '123', title: 'Course A'},
+    {_id: '123', title: 'Course B'},
+    {_id: '123', title: 'Course C'}
+  ];
+  addCourse = (title) =>
+    this.courses.push({_id: '234', title})
+  deleteCourse = (deletedCourse) =>
+    this.courses = this.courses.filter(course => course !== deletedCourse)
   ngOnInit(): void {
+    this.service.findAllCourses()
+      .then(courses => this.courses = courses);
   }
-
 }
