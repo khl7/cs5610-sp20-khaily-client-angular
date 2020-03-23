726db795339ce0d163dd82d469869f487f3ea58a
khaily <ly.k@husky.neu.edu>
2020-03-23 14:09:29 -0700

    added routing for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CourseTableComponent } from "./course-table/course-table.component";
+import { CourseViewerComponent } from "./course-viewer/course-viewer.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: CourseTableComponent },
+  { path: "course-viewer/:courseId", component: CourseViewerComponent },
+  {
+    path: "course-viewer/:courseId/modules/:moduleId",
+    component: CourseViewerComponent
+  },
+  {
+    path: "course-viewer/:courseId/modules/:moduleId/lessons/:lessonId",
+    component: CourseViewerComponent
+  },
+  {
+    path:
+      "course-viewer/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId",
+    component: CourseViewerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './course-list/course-list.component';
-import { CourseTableComponentComponent } from './course-table-component/course-table-component.component';
-import { CourseViewerComponentComponent } from './course-viewer-component/course-viewer-component.component';
-import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
-import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tabs-component.component';
 import { CourseTableComponent } from './course-table/course-table.component';
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { ModuleListComponent } from './module-list/module-list.component';
@@ -16,11 +11,6 @@ import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    CourseTableComponentComponent,
-    CourseViewerComponentComponent,
-    ModuleListComponentComponent,
-    LessonTabsComponentComponent,
     CourseTableComponent,
     CourseViewerComponent,
     ModuleListComponent,
