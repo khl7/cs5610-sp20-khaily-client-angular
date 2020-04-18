e876ec784abe1ca641ff015a3825534e9afdd2e3
khaily <ly.k@husky.neu.edu>
2020-04-17 18:28:09 -0700

    updated redeployed url

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = qid =>
-    fetch(`https://cs5610-sp20-kl-node-server.herokuapp.com/api/quizzes/${qid}/questions`).then(response =>
+    fetch(`https://cs5610-sp20-kl-a8-server-node.herokuapp.com/api/quizzes/${qid}/questions`).then(response =>
       response.json()
     );
 }
--- src/app/services/QuizzesServiceClient.ts
+++ src/app/services/QuizzesServiceClient.ts
@@ -2,11 +2,11 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch("https://cs5610-sp20-kl-node-server.herokuapp.com/api/quizzes").then(response =>
+    fetch("https://cs5610-sp20-kl-a8-server-node.herokuapp.com/api/quizzes").then(response =>
       response.json()
     );
   findQuizById = qid =>
-    fetch(`https://cs5610-sp20-kl-node-server.herokuapp.com/api/quizzes/${qid}`).then(response =>
+    fetch(`https://cs5610-sp20-kl-a8-server-node.herokuapp.com/api/quizzes/${qid}`).then(response =>
       response.json()
     );
 }
